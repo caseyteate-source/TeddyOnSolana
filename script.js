752ec7caf91411c728f87376635f75f942b66486
caseyteate-source <caseyteate@gmail.com>
2026-06-20 18:36:42 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
+const button = document.getElementById("enterBoard");
+const cards = document.querySelectorAll(".card");
 
+button.addEventListener("click", () => {
+  document.querySelector(".board").scrollIntoView({ behavior: "smooth" });
+});
+
+cards.forEach(card => {
+  card.addEventListener("click", () => {
+    alert(card.innerText + "\n\nEvidence file opening soon...");
+  });
+});
